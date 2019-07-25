096aae1453df9c025d0f6a7a5896f4cc22beaf03
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-07-26 00:56:06 +0900

    スクロールする桃太郎

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 function main() {
-  var scene = new g.Scene({game: g.game});
+  var scene = new g.Scene({
+    game: g.game,
+    assetIds: ["paper1"],
+  });
+  scene.loaded.add(function() {
+
+    var sprite = new g.Sprite({
+      scene: scene,
+      src: scene.assets["paper1"],
+    });
+    scene.append(sprite);
+    sprite.update.add(function() {
+      ++sprite.x;
+      sprite.modified();
+    })
+  });
   g.game.pushScene(scene);
 }
 
