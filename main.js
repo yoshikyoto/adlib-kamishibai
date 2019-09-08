a5855ff9bb878ac801e97aa9d16b064677312a49
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-09-09 03:37:04 +0900

    何故か文字が黒い

--- main.js
+++ main.js
@@ -6,13 +6,22 @@ function main() {
   const BGM_ASSET_ID = 'bgm'
   const TITLE_ASSET_ID = 'title'
   const SUB_TITLE = '桃太郎編A'
+  // 紙芝居のアセットIDたち
   const PAPER_ASSET_IDS = ['paper1', 'paper2', 'paper3', 'paper4']
+  // タイトルが表示されている時間
   const TITLE_SECOND = 10
+  // 説明文の枠
   const DESCRIPTION_FRAME_ASSET_ID = 'frame'
+  // ゲームの説明を表示している時間
   const DESCRIPTION_SECOND = 10
   // 紙芝居の1ページあたりの秒数
   const PAPER_INTERVAL = 10
+  // 終了時に流れるホイッスルの音
   const WHISTLE_SE_ASSET_ID = 'whistle'
+  // 数字のフォント画像
+  const NUMBERS_IMAGE_ASSET_ID = 'numbersBlack'
+  const NUMBERS_RED_IMAGE_ASSET_ID = 'numbersRed'
+  const CLOCK_IMAGE_ASSET_ID = 'clock'
 
   // 各シーンの生成
 
@@ -37,7 +46,15 @@ function main() {
     
     descriptionScene.setTimeout(() => {
       // 紙芝居シーン（最後のシーン）
-      const paperScene = createPaperScene(PAPER_INTERVAL, PAPER_ASSET_IDS, BGM_ASSET_ID, WHISTLE_SE_ASSET_ID)
+      const paperScene = createPaperScene(
+        PAPER_INTERVAL, 
+        PAPER_ASSET_IDS, 
+        BGM_ASSET_ID, 
+        WHISTLE_SE_ASSET_ID,
+        NUMBERS_IMAGE_ASSET_ID,
+        NUMBERS_RED_IMAGE_ASSET_ID,
+        CLOCK_IMAGE_ASSET_ID
+      )
       g.game.pushScene(paperScene)
 
     }, DESCRIPTION_SECOND * 1000) // ゲーム説明の表示されている時間
@@ -141,20 +158,47 @@ function createDescriptionScene(descriptionFrameAssetId) {
 /**
  * 紙芝居のシーンを作成して返す
  */
-function createPaperScene(paperInterval, paperAssetIds, bgmAssetId, whistleSeAssetId) {
-  const assetIds = paperAssetIds.concat([bgmAssetId]).concat([whistleSeAssetId])
+function createPaperScene(
+  paperInterval, 
+  paperAssetIds, 
+  bgmAssetId, 
+  whistleSeAssetId,
+  numbersImageAssetId,
+  numbersRedImageAssetId,
+  clockImageAssetId
+) {
+  const assetIds = paperAssetIds.concat([bgmAssetId, whistleSeAssetId])
+    .concat([numbersImageAssetId, numbersRedImageAssetId, clockImageAssetId])
   const scene = new g.Scene({
     game: g.game,
     assetIds: assetIds,
   })
   scene.loaded.add(function() {
     const bgm = scene.assets[bgmAssetId].play()
-    autoPaper(scene, paperInterval, paperAssetIds, bgm, whistleSeAssetId)
+    // BitmapFont はここで生成しておく
+    const width = 28
+    const height = 32
+    const glyphMap = getCountFontGlyphMap(width, height)
+    const numberFont = new g.BitmapFont({
+      src: scene.assets[numbersImageAssetId],
+      map: glyphMap,
+      defaultGlyphWidth: width,
+      defaultGlyphHeight: height,
+      missingGlyph: {}
+    })
+    const numberRedFont = new g.BitmapFont({
+      src: scene.assets[numbersRedImageAssetId],
+      map: glyphMap,
+      defaultGlyphWidth: width,
+      defaultGlyphHeight: height,
+      missingGlyph: {}
+    })
+    autoPaper(scene, paperInterval, paperAssetIds, bgm, whistleSeAssetId, numberFont, numberRedFont, clockImageAssetId)
   })
   return scene
 }
 
-function autoPaper(scene, interval, paperAssetIds, bgm, whistleSeAssetId) {
+function autoPaper(scene, interval, paperAssetIds, bgm, whistleSeAssetId, numberFont, numberRedFont, clockImageAssetId) {
   if (paperAssetIds.length === 0) {
     // すべての紙芝居が終了したときの処理
     bgm.stop()
@@ -167,12 +211,18 @@ function autoPaper(scene, interval, paperAssetIds, bgm, whistleSeAssetId) {
     src: scene.assets[paperAssetId],
   })
   scene.append(paper)
-  startTimer(scene, interval, () => {
-    autoPaper(scene, interval, paperAssetIds, bgm, whistleSeAssetId)
+  startTimer(scene, interval, numberFont, numberRedFont, clockImageAssetId, () => {
+    autoPaper(scene, interval, paperAssetIds, bgm, whistleSeAssetId, numberFont, numberRedFont, clockImageAssetId)
   })
 }
 
-function startTimer(scene, second, onFinish) {
+function startTimer(scene, second, numberFont, numberRedFont, clockImageAssetId, onFinish) {
+  const clock = new g.Sprite({
+    scene: scene,
+    src: scene.assets[clockImageAssetId],
+    x: g.game.width - 120,
+  })
+  scene.append(clock)
   let timerFont = new g.DynamicFont({
     game: g.game,
     fontFamily: g.FontFamily.SansSerif,
@@ -180,11 +230,12 @@ function startTimer(scene, second, onFinish) {
   })
   const timerLabel = new g.Label({
     scene: scene,
-    font: timerFont,
+    font: numberRedFont,
     text: String(second),
-    fontSize: 15,
+    fontSize: 36,
     textColor: "black",
     textAlign: g.TextAlign.Right,
+    x: g.game.width - 70
   })
   scene.append(timerLabel)
 
@@ -200,6 +251,26 @@ function startTimer(scene, second, onFinish) {
       onFinish()
     }
   }, 1000)
+}
+
+function getCountFontGlyphMap(width, height) {
+  const map = {}
+  for (var i = 0; i < 10; i++) {
+    // 0 .. 9 の順に並んでいると簡単だが 1 .. 9 0 の順で並んでいるので
+    // 0 (charaxterCode = 48) だけ特別扱いする必要がある
+    var characterCode = i + 49
+    if (characterCode == 58) {
+      characterCode = 48
+    }
+    const key = String(characterCode)
+    map[key] = {
+      width: width,
+      height: height,
+      x: i * width,
+      y: 0,
+    }
+  }
+  return map
 }
 
 module.exports = main
