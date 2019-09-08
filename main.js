8a43300d3b5e998e11c6fa02038a06097b64f247
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2019-09-08 10:58:17 +0900

    タイトルシーン仮作成

--- main.js
+++ main.js
@@ -2,15 +2,57 @@
  * エントリーポイント
  */
 function main() {
+  // 紙芝居の1ページあたりの秒数
+  const PAPER_INTERVAL = 10
+
+  // タイトルシーンを作成
+  let titleScene = createTitleScene()
+  g.game.pushScene(titleScene)
+  
+  // 紙芝居シーンを作成
+  let paperScene = createPaperScene(PAPER_INTERVAL)
+  //g.game.pushScene(paperScene)
+}
+
+/**
+ * タイトルのシーンを作成して返す
+ */
+function createTitleScene() {
+  const scene = new g.Scene({
+    game: g.game,
+  })
+  // タイトル
+  scene.loaded.add(function() {
+    const timerLabel = new g.Label({
+      scene: scene,
+      font: new g.DynamicFont({
+        game: g.game,
+        fontFamily: g.FontFamily.SansSerif,
+        size: 15,
+      }),
+      text: 'アドリブ紙芝居',
+      fontSize: 15,
+      textColor: "black",
+      textAlign: g.TextAlign.Center,
+   });
+   scene.append(timerLabel);
+  })
+  return scene
+}
+
+/**
+ * 紙芝居のシーンを作成して返す
+ */
+function createPaperScene(paperInterval) {
   let paperAssetIds = ["paper1", "paper2", "paper3", "paper4"];
   let scene = new g.Scene({
     game: g.game,
     assetIds: paperAssetIds,
   });
   scene.loaded.add(function() {
-    autoPaper(scene, 10, paperAssetIds);
+    autoPaper(scene, paperInterval, paperAssetIds);
   });
-  g.game.pushScene(scene);
+  return scene
 }
 
 function autoPaper(scene, interval, paperAssetIds) {
